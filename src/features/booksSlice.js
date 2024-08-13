bd4937add2678187e9fc8ca46add9a845a0184f2
Tuan Anh <anhvdt18@gmail.com>
2024-08-13 20:17:40 +0700

    A

--- src/features/booksSlice.js
+++ src/features/booksSlice.js
@@ -1,16 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../apiService";
 
-export const newBooksList = createAsyncThunk(
-  "bookList/newBookList",
-  async (state) => {
-    let url = `/books?_page=${state.pageNum}&_limit=${state.limit}`;
-    if (state.query) url += `&q=${state.query}`;
-    const response = await api.get(url);
-    return response.data;
-  }
-);
-
 const initialState = {
   books: [],
   pageNum: 1,
@@ -37,15 +27,14 @@ export const booksSlice = createSlice({
   },
 });
 
-// export const { setPageNums } = booksSlice.actions;
-// export const selectBooks = (state) => state.bookList.books;
 export default booksSlice.reducer;
 
-//         id: payload.id,
-//         imageLink: payload.imageLink,
-//         title: payload.title,
-//         author: payload.author,
-//         year: payload.year,
-//         country: payload.country,
-//         pages: payload.pages,
-//         language: payload.language,
+export const newBooksList = createAsyncThunk(
+  "bookList/newBookList",
+  async (state) => {
+    let url = `/books?_page=${state.pageNum}&_limit=${state.limit}`;
+    if (state.query) url += `&q=${state.query}`;
+    const response = await api.get(url);
+    return response.data;
+  }
+);
